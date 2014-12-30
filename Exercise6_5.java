0fb8821131f635ddb7f38e24fd464438c8f8092d
BBK-PiJ-2014-33 <lulu@liudmilas-mbp.dlink>
2014-12-30 13:18:17 +0300

    impl isSymmetrical for two-dimensional array

--- Exercise6_5.java
+++ Exercise6_5.java
@@ -2,6 +2,24 @@ public class Exercise6_5 {
     public static void main(String[] args)
     {
         MatrixChecker myMatrixChecker = new MatrixChecker();
+
+        //test isSymmetrical with two dimensional array
+        int [] [] myTestArray4 =new int[][]{
+                {1,2,3},
+                {2,4,5},
+                {3,5,1}
+        };
+
+        int [] [] myTestArray5 =new int[][]{
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        System.out.println(myMatrixChecker.isSymmetrical(myTestArray4));
+        System.out.println(myMatrixChecker.isSymmetrical(myTestArray5));
+
+        //test isSymmetrical with one dimensional array
         int [] myTestArray1 = {1,2,2,1};
         int [] myTestArray2 = {1,2,3,2,1};
         int [] myTestArray3 = {1,2,3,4};
@@ -9,6 +27,8 @@ public class Exercise6_5 {
         System.out.println(myMatrixChecker.isSymmetrical(myTestArray1));
         System.out.println(myMatrixChecker.isSymmetrical(myTestArray2));
         System.out.println(myMatrixChecker.isSymmetrical(myTestArray3));
+
+
 
     }
 }
--- MatrixChecker.java
+++ MatrixChecker.java
@@ -4,9 +4,8 @@ public class MatrixChecker
     public boolean isSymmetrical(int [] myArray)
     {
         Boolean checkResult = true;
-        int myArrayLength = myArray.length/2;
 
-        for ( int i = 0; i<myArrayLength; i++)
+        for ( int i = 0; i<myArray.length/2; i++)
         {
             if(myArray[i]!=myArray[myArray.length-i-1])
             {
@@ -14,5 +13,23 @@ public class MatrixChecker
             }
         }
         return checkResult;
+    }
+
+    public boolean isSymmetrical (int [][] myArray)
+    {
+        Boolean checkResult = true;
+
+        for(int i =0; i<myArray.length; i++)
+        {
+            for (int n = 0; n < myArray[0].length; n++)
+            {
+                if(myArray[i][n]!=myArray[n][i])
+                {
+                    checkResult=false;
+                }
+            }
+        }
+
+        return  checkResult;
     }
 }
